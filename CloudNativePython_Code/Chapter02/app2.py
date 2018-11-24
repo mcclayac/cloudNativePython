d6777df3ab0fc35bdcfc622dd5d0551cb0f8589b
Tony <anthony.mcclay@me.com>
2018-11-24 08:20:00 -0600

    Added USers and updated the database

--- CloudNativePython_Code/Chapter02/app2.py
+++ CloudNativePython_Code/Chapter02/app2.py
@@ -1,10 +1,60 @@
-from flask import Flask
-from flask import jsonify
+from flask import Flask, request, jsonify
+from flask import abort
+from flask import make_response, url_for
 import json
+from time import gmtime, strftime
 import sqlite3
 
 
 app = Flask(__name__)
+
+def list_users():
+    conn = sqlite3.connect('mydb.db')
+    print ("Opened database successfully");
+    api_list=[]
+    cursor = conn.execute("SELECT username, full_name,  email, password, id from users")
+    for row in cursor:
+        a_dict = {}
+        a_dict['username'] = row[0]
+        a_dict['name'] = row[1]
+        a_dict['email'] = row[2]
+        a_dict['password'] = row[3]
+        a_dict['id'] = row[4]
+        api_list.append(a_dict)
+
+    conn.close()
+    return jsonify({'user_list': api_list})
+
+def list_user(user_id):
+    conn = sqlite3.connect('mydb.db')
+    print ("Opened database successfully");
+    api_list=[]
+    cursor=conn.cursor()
+    cursor.execute("SELECT * from users where id=?",(user_id,))
+    data = cursor.fetchall()
+    print (data)
+    if len(data) == 0:
+        abort(404)
+    else:
+
+        user = {}
+        user['username'] = data[0][0]
+        user['name'] = data[0][1]
+        user['email'] = data[0][2]
+        user['password'] = data[0][3]
+        user['id'] = data[0][4]
+
+    conn.close()
+    return jsonify(user)
+
+
+# -----------------------------------------------------
+# -----------------------------------------------------
+# -----------------------------------------------------
+# -----------------------------------------------------
+
+
+
 
 @app.route("/api/v1/info")
 def home_index():
@@ -22,6 +72,15 @@ def home_index():
     conn.close()
     return jsonify({'api_version': api_list}), 200
 
+
+
+@app.route('/api/v1/users', methods=['GET'])
+def get_users():
+    return list_users()
+
+@app.route('/api/v1/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    return list_user(user_id)
 
 
 
